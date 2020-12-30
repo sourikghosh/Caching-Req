b4cd4edc06407239def183b85c4f377dccb3309a
Sourik Ghosh <sourikghosh31@gmail.com>
2020-12-30 20:29:08 +0530

    modified

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,19 @@
 import router, { Request, Response, NextFunction } from "express"
-
+import client from './db'
+import axios from 'axios'
 const routes = router()
 routes.get('/', (req: Request, res: Response, next: NextFunction) => {
     res.send({
         message: '☘'
     })
+}).post('/api/', async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const result = await axios.get('')
+        client.set('', '')
+
+    } catch (err) {
+        next(err)
+    }
 })
 
 export default routes

3fc438eaa67b38e7270a3cbfa4cc10cd481a4b17
Sourik Ghosh <sourikghosh31@gmail.com>
2020-12-30 22:55:00 +0530

    improved

--- src/routes.ts
+++ src/routes.ts
@@ -1,16 +1,39 @@
 import router, { Request, Response, NextFunction } from "express"
-import client from './db'
+import { asyncGet, asyncSetex } from './db'
 import axios from 'axios'
 const routes = router()
-routes.get('/', (req: Request, res: Response, next: NextFunction) => {
+routes.get('/', async (req: Request, res: Response, next: NextFunction) => {
     res.send({
         message: '☘'
     })
-}).post('/api/', async (req: Request, res: Response, next: NextFunction) => {
+}).get('/api/user', async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const result = await axios.get('')
-        client.set('', '')
-
+        const result = await asyncGet('user')
+        if (result) {
+            console.log('using cached data')
+            res.send(JSON.parse(result))
+            return
+        }
+        const respone = await axios.get('https://jsonplaceholder.typicode.com/users')
+        const saveResult = await asyncSetex('user', 5, JSON.stringify(respone.data))
+        console.log('new data cached', saveResult)
+        res.send(respone.data)
+    } catch (err) {
+        next(err)
+    }
+}).get('/api/user/:user_id', async (req, res, next) => {
+    try {
+        const { user_id } = req.params
+        const result = await asyncGet(user_id)
+        if (result) {
+            console.log('using cached data')
+            res.send(JSON.parse(result))
+            return
+        }
+        const respone = await axios.get(`https://jsonplaceholder.typicode.com/users/${user_id}`)
+        const cacheResult = await asyncSetex(user_id, 5, JSON.stringify(respone.data))
+        console.log('new data cached', cacheResult)
+        res.send(respone.data)
     } catch (err) {
         next(err)
     }
